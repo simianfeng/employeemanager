9bc6aa260fcffcf9b4f2cb88fc5de5eb323b19d0
simianfeng <56980681+simianfeng@users.noreply.github.com>
2022-01-24 16:48:04 -0500

    add backend component

---
+++ src/main/java/simon/feng/employeemanager/exception/UserNotFoundExecption.java
@@ -0,0 +1,7 @@
+package simon.feng.employeemanager.exception;
+
+public class UserNotFoundExecption extends RuntimeException{
+    public UserNotFoundExecption(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/simon/feng/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,13 @@
+package simon.feng.employeemanager.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import simon.feng.employeemanager.model.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
